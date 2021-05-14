a650dd76912ba857c1f6dada63fa9fa15201fd7d
sofia.wahlmark.0513 <so0513wa-s@student.lu.se>
2021-05-14 21:02:04 +0200

    fixes, not tested

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         MarkovCainCreator markovCainCreator = new MarkovCainCreator(System.in);
+        System.out.println(markovCainCreator.textGenerator());
     }
 }
--- src/MarkovCainCreator.java
+++ src/MarkovCainCreator.java
@@ -1,18 +1,22 @@
 import java.io.File;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class MarkovCainCreator {
+    private Scanner scanner;
+    private int maxLength = 280;
     private String firstWord;
     private String secondWord;
+    private String thirdWord;
     private String combinedWords;
-    private Map<String, ArrayList<String>> wordMap = new HashMap<>();
-    private Scanner scanner;
+    private Map<String, ArrayList<String>> wordMap = new HashMap<>(); // ett ord -> två ord, det sista av de två orden -> nästa två ord, osv
+    private List<String> sentenceStarters = new ArrayList<>();
     private String currentString;
+    private String nextString;
     private String resultString;
+    private String backupResultString;
+    private ArrayList<String> possibilities;
+    private StringBuffer sb;
 
     public MarkovCainCreator (InputStream file) {
         scanner = new Scanner(file);
@@ -20,33 +24,85 @@ public class MarkovCainCreator {
 
     public void MarkovChain() {
         firstWord = scanner.next();
+        sentenceStarters.add(firstWord);
 
         while (scanner.hasNext()) {
             secondWord = scanner.next();
-            if (secondWord.endsWith(".")) { // ???
-                break;
+
+            if (firstWord.endsWith(".")) {
+                sentenceStarters.add(firstWord); //om föregående ord slutar med punkt är nästa ord en meningsstartare
             }
 
-            combinedWords = firstWord + " " + secondWord;
-
             if (scanner.hasNext()) {
-                firstWord = scanner.next();
+                thirdWord = scanner.next();
+                combinedWords = secondWord + " " + thirdWord;
 
-                if (!wordMap.containsKey(combinedWords)) {
-                    wordMap.put(combinedWords, new ArrayList<>());
+                if (!wordMap.containsKey(firstWord)) {
+                    wordMap.put(firstWord, new ArrayList<>());
                 }
-                wordMap.get(combinedWords).add(firstWord);
+                wordMap.get(firstWord).add(combinedWords);
+                firstWord = thirdWord;
             }
         }
     }
 
-    public void print() {
-        currentString = "ett ord";
+    public String textGenerator() {
+        currentString = sentenceStarters.get((int)(Math.random() * sentenceStarters.size())); // get random sentence starter
         resultString = currentString;
 
-        // for sålänge mindre än 280 tecken
-        // Array possiblities = de två sista ordens värde
-        // next = välj en random
-        // resultString += next
+        while (resultString.length() <= maxLength) {
+            backupResultString = resultString; // keeping a backup of resultString in case text gets too long
+            possibilities = wordMap.get(currentString);
+            nextString = possibilities.get((int)(Math.random() * possibilities.size())); //väljer en random
+            resultString += " " + nextString;
+
+            if (resultString.length() > maxLength) {
+                if (backupResultString.endsWith(".")) {
+                    sb = new StringBuffer(backupResultString);
+                    sb.deleteCharAt(sb.length() - 1);
+                    backupResultString = sb.toString();
+                }
+                return backupResultString;
+            }
+
+            currentString = resultString.substring(resultString.lastIndexOf(" ")+1);;
+        }
+
+        if (resultString.endsWith(".")) {
+            sb = new StringBuffer(resultString);
+            sb.deleteCharAt(sb.length()-1);
+            resultString = sb.toString();
+        }
+        return resultString;
     }
+
+    /*
+    public void recursiveTextGenerator() {
+
+    }
+
+    public boolean recursiveTextGeneratorHelper(String resultString) {
+        if (resultString.length() > maxLength) {
+            return false;
+        }
+
+        if (recursiveTextGeneratorHelper(resultString)) {
+
+        }
+        if (row == 9) {
+            return true;
+        }
+        if (matrix[row][col] != 0) { //kolla om ifylld siffra
+            return solve(row, col + 1);
+        }
+        for (int number = 1; number < 10; number++) {
+            if (put(number, row, col)) {
+                if (solve(row, col + 1)) {
+                    return true;
+                }
+            }
+        }
+        remove(row, col);
+        return false;
+    }*/
 }
