3894db44743a3f93b61d10dd58f4d697bf07d955
sofwah <[sofia.wahlmark@gmail.com]>
2021-06-08 15:24:04 +0200

    added comments with potential future fixes

--- src/MarkovChainCreator.java
+++ src/MarkovChainCreator.java
@@ -4,25 +4,17 @@ import java.util.*;
 public class MarkovChainCreator {
     private Scanner scanner;
     private int maxLength = 280;
-    private String firstWord;
-    private String secondWord;
-    private String thirdWord;
-    private String combinedWords;
-    private Map<String, ArrayList<String>> wordMap = new HashMap<>(); // ett ord -> två ord, det sista av de två orden -> nästa två ord, osv
+    private String firstWord, secondWord, thirdWord, combinedWords, currentString, nextString, resultString, returnString;
+    private Map<String, ArrayList<String>> wordMap = new HashMap<>(); // one word -> two words, the last out of the two words -> the next two words, and so on
     private List<String> sentenceStarters = new ArrayList<>();
-    private String currentString;
-    private String nextString;
-    private String resultString;
     private ArrayList<String> possibilities;
     private StringBuffer sb;
-    private String returnString;
 
     public MarkovChainCreator(InputStream file) {
         scanner = new Scanner(file);
     }
 
     public void createMarkovChain() {
-
         firstWord = scanner.next();
 
         while (scanner.hasNext()) {
@@ -30,6 +22,7 @@ public class MarkovChainCreator {
 
             if (scanner.hasNext()) {
                 thirdWord = scanner.next();
+                //only the last word in combinedWords is allowed to end with .!?
                 if (secondWord.endsWith(".") || secondWord.endsWith("!") || secondWord.endsWith("?")) {
                     combinedWords = secondWord;
                 } else {
@@ -43,18 +36,19 @@ public class MarkovChainCreator {
 
                 if (firstWord.endsWith(".") || firstWord.endsWith("!") || firstWord.endsWith("?")) {
                     sentenceStarters.add(secondWord); //om föregående ord slutar med punkt är nästa ord en meningsstartare
-                }
+                } //this should maybe be moved to earlier in the loop
 
                 firstWord = thirdWord;
             }
         }
+        //the above loop is currently after each loop giving firstWord the value of thirdWord, could be changed to secondWord to get a better chain and more keys in the map
     }
 
     public String generateText() {
         returnString = generateTextHelper();
-        while ((resultString.split(" ").length <= 1) || (resultString.split(" ").length > 280)) {
+        while ((resultString.split(" ").length <= 1) || (resultString.split(" ").length > 280)) { //redo until appropriate length
             returnString = generateTextHelper();
-            if ((resultString.split(" ").length <= 280) && (Math.random() < 0.1)) { // sometimes lets through one word outputs (10% of the times)
+            if ((resultString.split(" ").length <= 280) && (Math.random() < 0.1)) { // sometimes lets through one word outputs (10% chance)
                 break;
             }
         }
@@ -71,16 +65,16 @@ public class MarkovChainCreator {
                 break;
             }
 
-            possibilities = wordMap.get(currentString);
+            possibilities = wordMap.get(currentString); //list of possible following words
             if (currentString.endsWith(".") || currentString.endsWith("?") || currentString.endsWith("!")) {
-                nextString = sentenceStarters.get((int)(Math.random() * sentenceStarters.size())); // get random sentence starter
+                nextString = sentenceStarters.get((int)(Math.random() * sentenceStarters.size())); // get random sentence starter to start new sentence
             } else {
                 nextString = possibilities.get((int)(Math.random() * possibilities.size())); //chooses a random out of possible next words
             }
 
             resultString += " " + nextString;
 
-            if (resultString.length() > maxLength) { // if over max length, break and the string will be re-done through the textGenerator method
+            if (resultString.length() > maxLength) { // if over max length, break and the string will be re-done in the textGenerator method
                 break;
             }
 
@@ -93,7 +87,7 @@ public class MarkovChainCreator {
                 break;
             }
 
-            currentString = resultString.substring(resultString.lastIndexOf(" ") + 1);
+            currentString = resultString.substring(resultString.lastIndexOf(" ") + 1); //gets last word
         }
 
         if (resultString.endsWith(".")) { // deletes punctuation in the end to fit tweet theme
